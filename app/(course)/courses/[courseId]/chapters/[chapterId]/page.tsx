820da3e68a9bb2bc917e2a10f0b5d7b659832005
Sakil Khan <sakilk130@gmail.com>
2023-11-09 00:24:43 +0600

    feat: course description preview

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,9 +1,13 @@
 import { redirect } from 'next/navigation';
+import { File } from 'lucide-react';
 
 import { getChapter } from '@/actions/get-chapter';
 import { Banner } from '@/components/banner';
+import { Separator } from '@/components/ui/separator';
 import { auth } from '@clerk/nextjs';
+import { CourseEnrollButton } from './components/course-enroll-button';
 import { VideoPlayer } from './components/video-player';
+import { Preview } from '@/components/preview';
 
 const ChapterIdPage = async ({
   params,
@@ -37,7 +41,7 @@ const ChapterIdPage = async ({
 
   const isLocked = !chapter.isFree && !purchase;
   const completeOnEnd = !!purchase && !userProgress?.isCompleted;
-
+  console.log(attachments);
   return (
     <div>
       {userProgress?.isCompleted && (
@@ -60,6 +64,39 @@ const ChapterIdPage = async ({
             isLocked={isLocked}
             completeOnEnd={completeOnEnd}
           />
+        </div>
+        <div>
+          <div className="flex flex-col items-center justify-between p-4 md:flex-row">
+            <h2 className="mb-2 text-2xl font-semibold">{chapter.title}</h2>
+            {purchase ? null : (
+              <CourseEnrollButton
+                courseId={params.courseId}
+                price={course.price!}
+              />
+            )}
+          </div>
+          <Separator />
+          <div>
+            <Preview value={chapter.description!} />
+          </div>
+          {!!attachments.length && (
+            <>
+              <Separator />
+              <div className="p-4">
+                {attachments.map((attachment) => (
+                  <a
+                    href={attachment.url}
+                    target="_blank"
+                    key={attachment.id}
+                    className="flex items-center w-full p-3 border rounded-md bg-sky-200 text-sky-700 hover:underline"
+                  >
+                    <File />
+                    <p className="line-clamp-1">{attachment.name}</p>
+                  </a>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
